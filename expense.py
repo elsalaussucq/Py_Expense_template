2484f62870a4c4271d51f57bf8a6fa95a89744b9
Elsa Laussucq <elsa.laussucq@epita.fr>
2022-11-02 23:13:48 +0100

    [ADD][Expense] Add of the continuation of the tp

--- expense.py
+++ expense.py
@@ -24,26 +24,40 @@ def open_csv():
     users = []
     for row in csvreader:
         users.append({"name" : row[0]})
-    user_option = {
+    user = {
         "type":"checkbox",
+        "name":"user",
+        "message":"Expense Spender",
+        "choices": users
+    }
+    option = []
+    option = prompt(user)
+    return option
+
+def open_csv_spender(): 
+    file = open('users.csv')
+    csvreader = csv.reader(file)
+    users = []
+    for row in csvreader:
+        users.append(row[0])
+    user_options = {
+        "type":"list",
         "name":"user_options",
         "message":"Expense Spender",
         "choices": users
     }
     option = []
-    option = prompt(user_option)
- #   val = confirm("Do you want to continue?")
- #   while val == True:
- #       option.append(prompt(user_option))
+    option = prompt(user_options)
     return option
 
 
 def new_expense(*args):
     infos = prompt(expense_questions)
     users = []
+    spender = open_csv_spender()
     users = open_csv()
     with open("expense_report.csv", "a") as file:
-        file.write('\n' + infos['amount'] + ',' + infos['label'] + ',' + str(users['user_options']) + ' ')
+        file.write('\n' + infos['amount'] + ',' + infos['label'] + ',' + spender['user_options'] + str(users['user']) + ' ')
         file.close()
     print("Expense Added !")
     return True
